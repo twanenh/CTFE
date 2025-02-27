2930fe35b4cffedbe77212fee9ebd3e8a8ff2a1f
twanenh <anhemnha8x@gmail.com>
2025-02-27 15:42:43 +0700

    updt

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -15,7 +15,7 @@ export class AuthService {
   }
 
   register(email: string, password: string): Observable<any> {
-    return this.http.post(`${this.baseUrl}/register`, { email, password });
+    return this.http.post(`${this.baseUrl}/register`, { email, password },{ withCredentials: true });
   }
 
   login(email: string, password: string): Observable<any> {
